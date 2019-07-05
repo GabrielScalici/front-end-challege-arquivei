bb65fc67564338729fbe964f998e9e44ab8bf4f6
GabrielScalici <gabrielhc.scalici@gmail.com>
2019-07-05 01:40:44 -0300

    last ajust chart

--- challege-front/src/components/ChartCustom/index.js
+++ challege-front/src/components/ChartCustom/index.js
@@ -23,16 +23,6 @@ const ChartCustom = (props) => {
     });
     console.log("items");
 
-    const data = [
-        { name: 'Page A', pv: 2400, amt: 2400 },
-        { name: 'Page B', pv: 1398, amt: 2210 },
-        { name: 'Page C', pv: 9800, amt: 2290 },
-        { name: 'Page D', pv: 3908, amt: 2000 },
-        { name: 'Page E', pv: 4800, amt: 2181 },
-        { name: 'Page F', pv: 3800, amt: 2500 },
-        { name: 'Page G', pv: 4300, amt: 2100 },
-    ];
-
     return (
         <LineChart
             width={props.width/1.3}
@@ -48,7 +38,7 @@ const ChartCustom = (props) => {
             />
             <CartesianGrid strokeDasharray='3 3' />
             <Tooltip />
-            <YAxis />
+            <YAxis/>
             <XAxis dataKey='name' />
             <Legend> Compras x Quantidade</Legend>
         </LineChart>
